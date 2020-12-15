ae4194f93f8de6e038024e45208493f0ba54eecc
idochetrit <idochetrit.dev@gmail.com>
2020-12-15 15:25:49 +0200

    refine jobs, cleanups, and add proper readme

--- src/scripts/get-secret-value.sh
+++
@@ -1,7 +0,0 @@
-echo "Retrieving .env file for $APP_NAME ($NODE_ENV)"
-
-if [ "$NODE_ENV" == "production" ]; then
-    aws secretsmanager get-secret-value --secret-id "$APP_NAME" > .env
-else
-    aws secretsmanager get-secret-value --secret-id "$APP_NAME-$NODE_ENV" > .env
-fi
