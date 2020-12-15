fe7a5d855dfdbaf9acc386ab56d8f0c0e32bf46b
idochetrit <idochetrit.dev@gmail.com>
2020-12-15 10:28:40 +0200

    [semver.major] adds script for collecting .env from secret-manager

---
+++ src/scripts/get-secret-value.sh
@@ -0,0 +1,7 @@
+echo "Retrieving .env file for $APP_NAME ($NODE_ENV)"
+
+if [ $NODE_ENV == "production" ]; then
+    aws secretsmanager get-secret-value --secret-id $APP_NAME > .env
+else
+    aws secretsmanager get-secret-value --secret-id "$APP_NAME-$NODE_ENV" > .env
+fi
